5dfb5e736f4466017a2cc2a31308a0ca7eb6d703
Linus Atte <linuseatte@gmail.com>
2023-11-28 21:44:42 -0800

    update

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/bin/python3
 def magic_calculation(a, b):
 	result = 98
 	result += a ** b
--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-str = str[39:67] + str[107:112] + str[:6]
+str = str[39:66] + str[106:112] + str[:6]
 print(str)
