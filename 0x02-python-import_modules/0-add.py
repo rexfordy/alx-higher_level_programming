9f3f621dbc8883bf77db875b9ba78f4f4f6b73e8
Linus Atte <linuseatte@gmail.com>
2023-12-02 12:59:57 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
-add_0 = __import__('add_0')
-print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+
+result = add(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, result))
