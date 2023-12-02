208f3bc79fdc1cb2d4dc23d90d6234e8fbcbb17b
Linus Atte <linuseatte@gmail.com>
2023-12-02 05:57:56 -0800

    Python Data Structure

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+#include <stdio.h>
+
+/**
+ * print_python_list_info - prints basic info about Python lists
+ * @p: PyObject (Python list)
+ */
+void print_python_list_info(PyObject *p)
+{
+    PyListObject *list = (PyListObject *)p;
+    Py_ssize_t size, alloc, i;
+    PyObject *item;
+
+    size = Py_SIZE(p);
+    alloc = list->allocated;
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", alloc);
+
+    for (i = 0; i < size; i++)
+    {
+        item = PyList_GetItem(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+    }
+}
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,3 @@
+This is my Readme file 
+For 0x03-python-data_structures
+Written by Linus Atte (linuseatte@gmail.com)
